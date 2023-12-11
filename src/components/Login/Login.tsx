5515ed4a8132d22af6be5e790eeb59c294909e27
Chris Berlant <chris.berlant@gmail.com>
2023-12-11 01:10:24 +0100

    added server status indicator on login

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -18,7 +18,7 @@ function Login() {
 	const [visible, { toggle: toggleOverlay }] = useDisclosure(false);
 	const navigate = useNavigate();
 	// Rediriger vers l'app si utilisateur déjà connecté
-	const { data: user } = useCheckLoginStatus();
+	const { data: user, isError, isLoading, error } = useCheckLoginStatus();
 	const form = useForm({
 		validate: zodResolver(userLoginSchema),
 		initialValues: {
@@ -32,48 +32,68 @@ function Login() {
 		if (user) navigate('/attributed-lines');
 	}, [user, navigate]);
 
-	return (
-		<main className={classes.loginPage}>
-			<div className={classes.wrapper}>
-				<Paper className={classes.form} radius={0} p={30}>
-					<Title
-						order={1}
-						className={classes.title}
-						ta='center'
-						mt='md'
-						mb={50}
-					>
-						Gestion SMAC - BETA
-					</Title>
-					<form onSubmit={form.onSubmit(() => submitLogin())}>
-						<LoadingOverlay
-							visible={visible}
-							zIndex={1000}
-							overlayProps={{ radius: 'sm', blur: 2 }}
-						/>
-						<TextInput
-							label='Email'
-							placeholder='Votre adresse mail'
-							size='md'
-							data-autofocus
-							{...form.getInputProps('email')}
-						/>
-						<PasswordInput
-							label='Mot de passe'
-							placeholder='Votre mot de passe'
+	if (isError || isLoading) {
+		return (
+			<main className={classes.loginPage}>
+				<div className={classes.wrapper}>
+					<Paper className={classes.form} radius={0} p={30}>
+						<Title
+							order={1}
+							className={classes.title}
+							ta='center'
 							mt='md'
-							size='md'
-							{...form.getInputProps('password')}
-						/>
+							mb={50}
+						>
+							Gestion SMAC - BETA
+						</Title>
+						<form onSubmit={form.onSubmit(() => submitLogin())}>
+							<LoadingOverlay
+								visible={visible}
+								zIndex={1000}
+								overlayProps={{ radius: 'sm', blur: 2 }}
+							/>
+							<TextInput
+								label='Email'
+								placeholder='Votre adresse mail'
+								size='md'
+								data-autofocus
+								{...form.getInputProps('email')}
+							/>
+							<PasswordInput
+								label='Mot de passe'
+								placeholder='Votre mot de passe'
+								mt='md'
+								size='md'
+								{...form.getInputProps('password')}
+							/>
 
-						<Button type='submit' fullWidth mt='xl' size='md'>
-							Connexion
-						</Button>
-					</form>
-				</Paper>
-			</div>
-		</main>
-	);
+							<Button type='submit' fullWidth mt='xl' size='md'>
+								Connexion
+							</Button>
+						</form>
+					</Paper>
+					{isError && (
+						<div className={classes.serverStatus}>
+							&Eacute;tat du serveur :{' '}
+							{error.message === 'Failed to fetch' ? (
+								<span className={classes.statusRed}>
+									<span className={classes.statusDot}></span>
+									Hors ligne
+								</span>
+							) : (
+								<span className={classes.statusGreen}>
+									<span
+										className={`${classes.statusDot} ${classes.statusDotAnimated}`}
+									></span>
+									En ligne
+								</span>
+							)}
+						</div>
+					)}
+				</div>
+			</main>
+		);
+	}
 }
 
 export default Login;
