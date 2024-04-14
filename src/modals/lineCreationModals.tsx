fefc3c1ff1afbc2257b434d80b2a9e615b02ef83
Chris Berlant <chris.berlant@gmail.com>
2024-04-14 17:54:29 +0200

    updated lineCreationModals to use openConfirmModal when device owner changed

--- src/modals/lineCreationModals.tsx
+++ src/modals/lineCreationModals.tsx
@@ -47,7 +47,7 @@ export default function displayLineCreationModal({
 			// Si le propriétaire reste le même
 			if (newOwnerId === currentOwnerId) {
 				return modals.openConfirmModal({
-					title: "Appareil déjà affecté à une autre ligne appartenant à l'agent",
+					title: "Appareil déjà affecté à une autre ligne de l'agent",
 					size: 'lg',
 					centered: true,
 					children: (
@@ -89,7 +89,7 @@ export default function displayLineCreationModal({
 			}
 
 			// Si l'ancien et le nouveau propriétaire sont différents, l'ancien pouvant être nul
-			return modals.open({
+			return modals.openConfirmModal({
 				title: 'Appareil déjà affecté à une autre ligne',
 				size: 'xl',
 				centered: true,
@@ -114,55 +114,27 @@ export default function displayLineCreationModal({
 							)}
 							.
 						</Text>
-						<Text>A qui souhaitez-vous affecter la ligne ?</Text>
-						<Flex align='center'>
-							<Button
-								mt='lg'
-								mx='md'
-								onClick={() => {
-									creationData.agentId =
-										currentOwnerId ?? null;
-									createLine({
-										data: creationData,
-										updateOldLine: true,
-									});
-									setValidationErrors({});
-									exitCreatingMode();
-									modals.closeAll();
-								}}
-							>
-								{currentOwnerId
-									? `Au propriétaire actuel ${currentOwnerFullName}`
-									: 'Créer la ligne sans propriétaire'}
-							</Button>
-							<Button
-								mt='lg'
-								mx='md'
-								color='rgba(68, 145, 42, 1)'
-								onClick={() => {
-									createLine({
-										data: creationData,
-										updateDevice: true,
-										updateOldLine: true,
-									});
-									setValidationErrors({});
-									exitCreatingMode();
-									modals.closeAll();
-								}}
-							>
-								À {newOwnerFullName}
-							</Button>
-						</Flex>
-						<Button
-							fullWidth
-							mt='xl'
-							variant='default'
-							onClick={() => modals.closeAll()}
-						>
-							Annuler
-						</Button>
+						<Text mb='xl'>
+							Si vous continuez, il sera désaffecté de cette ligne
+							{currentOwnerFullName
+								? 'et de son proprétaire'
+								: ''}
+							.
+						</Text>
 					</>
 				),
+				labels: { confirm: 'Confirmer', cancel: 'Annuler' },
+				onCancel: modals.closeAll,
+				onConfirm: () => {
+					createLine({
+						data: creationData,
+						updateOldLine: true,
+						updateDevice: true,
+					});
+					setValidationErrors({});
+					exitCreatingMode();
+					modals.closeAll();
+				},
 			});
 		}
 
@@ -318,8 +290,8 @@ export default function displayLineCreationModal({
 	}
 
 	// Si l'ancien et le nouveau propriétaire sont différents (non nuls)
-	return modals.open({
-		title: 'Appareil déjà affecté à un autre agent',
+	return modals.openConfirmModal({
+		title: "Changement de propriétaire de l'appareil",
 		size: 'xl',
 		centered: true,
 		children: (
@@ -330,53 +302,22 @@ export default function displayLineCreationModal({
 					<span className='bold-text'>{currentOwnerFullName}</span> et
 					n'est affecté à aucune ligne.
 				</Text>
-				<Text>
-					Voulez-vous le réaffecter à{' '}
-					<span className='bold-text'>{newOwnerFullName} </span>?
+				<Text mb='xl'>
+					Si vous continuez, il sera affecté automatiquement à l'agent{' '}
+					<span className='bold-text'>{newOwnerFullName}</span>.
 				</Text>
-				<Flex align='center'>
-					<Button
-						mt='lg'
-						mx='md'
-						onClick={() => {
-							creationData.agentId = currentOwnerId;
-							createLine({
-								data: creationData,
-							});
-							setValidationErrors({});
-							exitCreatingMode();
-							modals.closeAll();
-						}}
-					>
-						Affecter la ligne au propriétaire actuel{' '}
-						{currentOwnerFullName}
-					</Button>
-					<Button
-						mt='lg'
-						mx='md'
-						color='rgba(68, 145, 42, 1)'
-						onClick={() => {
-							createLine({
-								data: creationData,
-								updateDevice: true,
-							});
-							setValidationErrors({});
-							exitCreatingMode();
-							modals.closeAll();
-						}}
-					>
-						Confirmer la réaffectation à {newOwnerFullName}
-					</Button>
-				</Flex>
-				<Button
-					fullWidth
-					mt='xl'
-					variant='default'
-					onClick={() => modals.closeAll()}
-				>
-					Annuler
-				</Button>
 			</>
 		),
+		labels: { confirm: 'Confirmer', cancel: 'Annuler' },
+		onCancel: modals.closeAll,
+		onConfirm: () => {
+			createLine({
+				data: creationData,
+				updateDevice: true,
+			});
+			setValidationErrors({});
+			exitCreatingMode();
+			modals.closeAll();
+		},
 	});
 }
