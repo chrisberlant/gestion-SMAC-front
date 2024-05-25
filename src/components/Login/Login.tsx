8a9e8e3a0f431029b6685692a8d9a9c2c720fc9b
Chris Berlant <chris.berlant@gmail.com>
2024-05-25 22:53:01 +0200

    fix: Login.tsx now renders the component before getting user login status

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -17,12 +17,7 @@ import classes from './login.module.css';
 export default function Login() {
 	const [visible, { toggle: toggleOverlay }] = useDisclosure(false);
 	const navigate = useNavigate();
-	const {
-		data: user,
-		isError: isNotConnected,
-		isLoading,
-		error,
-	} = useCheckLoginStatus();
+	const { data: userIsConnected, isLoading, error } = useCheckLoginStatus();
 
 	const form = useForm({
 		validate: zodResolver(userLoginSchema),
@@ -36,90 +31,76 @@ export default function Login() {
 
 	useEffect(() => {
 		// Rediriger vers l'app si utilisateur déjà connecté
-		if (user) navigate('/lines');
-	}, [user, navigate]);
+		if (userIsConnected) navigate('/lines');
+	}, [userIsConnected, navigate]);
 
 	return (
-		<>
-			{isNotConnected && (
-				<main className={classes.loginPage}>
-					<div className={classes.wrapper}>
-						<Paper className={classes.form} radius={0} p={30}>
-							<Title
-								order={1}
-								className={classes.title}
-								ta='center'
-								mt='md'
-								mb={50}
-							>
-								Gestion SMAC - Connexion
-							</Title>
-							<form onSubmit={form.onSubmit(() => submitLogin())}>
-								<LoadingOverlay
-									visible={visible}
-									zIndex={1000}
-									overlayProps={{ radius: 'sm', blur: 2 }}
-								/>
-								<TextInput
-									label='Email'
-									placeholder='Votre adresse mail'
-									data-autofocus
-									{...form.getInputProps('email')}
-									size='md'
-									labelProps={{ mb: '4' }}
-								/>
-								<PasswordInput
-									label='Mot de passe'
-									placeholder='Votre mot de passe'
-									mt='md'
-									{...form.getInputProps('password')}
-									size='md'
-									labelProps={{ mb: '4' }}
-								/>
+		<main className={classes.loginPage}>
+			<div className={classes.wrapper}>
+				<Paper className={classes.form} radius={0} p={30}>
+					<Title
+						order={1}
+						className={classes.title}
+						ta='center'
+						mt='md'
+						mb={50}
+					>
+						Gestion SMAC - Connexion
+					</Title>
+					<form onSubmit={form.onSubmit(() => submitLogin())}>
+						<LoadingOverlay
+							visible={visible}
+							zIndex={1000}
+							overlayProps={{ radius: 'sm', blur: 2 }}
+						/>
+						<TextInput
+							label='Email'
+							placeholder='Votre adresse mail'
+							data-autofocus
+							{...form.getInputProps('email')}
+							size='md'
+							labelProps={{ mb: '4' }}
+						/>
+						<PasswordInput
+							label='Mot de passe'
+							placeholder='Votre mot de passe'
+							mt='md'
+							{...form.getInputProps('password')}
+							size='md'
+							labelProps={{ mb: '4' }}
+						/>
 
-								<Button
-									type='submit'
-									fullWidth
-									mt='xl'
-									size='md'
-								>
-									Connexion
-								</Button>
-							</form>
+						<Button type='submit' fullWidth mt='xl' size='md'>
+							Connexion
+						</Button>
+					</form>
 
-							<div className={classes.serverStatus}>
-								&Eacute;tat du serveur :{' '}
-								{isLoading ? (
-									<span>Vérification en cours...</span>
+					<div className={classes.serverStatus}>
+						&Eacute;tat du serveur :{' '}
+						{isLoading ? (
+							<span>Vérification en cours...</span>
+						) : (
+							<>
+								{error?.message === 'Failed to fetch' ? (
+									<span className={classes.statusRed}>
+										<span
+											className={classes.statusDot}
+										></span>
+										Hors ligne
+									</span>
 								) : (
-									<>
-										{error?.message ===
-										'Failed to fetch' ? (
-											<span className={classes.statusRed}>
-												<span
-													className={
-														classes.statusDot
-													}
-												></span>
-												Hors ligne
-											</span>
-										) : (
-											<span
-												className={classes.statusGreen}
-											>
-												<span
-													className={`${classes.statusDot} ${classes.statusDotAnimated}`}
-												></span>
-												En ligne
-											</span>
-										)}
-									</>
+									<span className={classes.statusGreen}>
+										<span
+											className={`${classes.statusDot} ${classes.statusDotAnimated}`}
+										></span>
+										En ligne
+									</span>
 								)}
-							</div>
-						</Paper>
+							</>
+						)}
 					</div>
-				</main>
-			)}
-		</>
+				</Paper>
+			</div>
+		</main>
 	);
 }
